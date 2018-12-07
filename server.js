47f8bfaebcd409e1748ff2ff40f12cc8a844be6a
Richard Bellamy <richard.bellamy123@gmail.com>
2018-12-06 19:18:09 -0800

    feat 1 working

--- server.js
+++ server.js
@@ -4,19 +4,34 @@ require('dotenv').config();
 const express = require('express');
 //app
 const app = express();
+
+const cors = require('cors');
 // port
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./public'));
+// app.use(express.static('./public'));
 
-app.get('/hello', (request, response) =>{
-  response.status(200).send('hello');
-})
+// app.get('/hello', (request, response) =>{
+//   response.status(200).send('hello');
+// })
 //get location
+app.use(cors());
 app.get('/location', (request, response) => {
   const locationData = searchToLatLong(request.query.data || 'Lynnwood, WA');
+  response.send(locationData);
 });
 
+function searchToLatLong(query){
+  const geoData = require('./geo.json');
+  const location = new Location(geoData.results[0]);
+  return location;
+}
+
+function Location(location){
+  this.formatted_query = location.formatted_address;
+  this.latitude = location.geometry.location.lat;
+  this.longitude = location.geometry.location.lng;
+}
 //errors
 app.use('/*', (req, res) =>{
   res.status(400).send(`sorry that didn't work`);
