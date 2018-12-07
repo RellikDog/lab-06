8af347ccb20f7013a979689a089eeebcaa402986
Richard Bellamy <richard.bellamy123@gmail.com>
2018-12-06 21:14:23 -0800

    fixme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # lab-06
+
+Number and name of feature:feature 1 render map
+
+Estimate of time needed to complete: 1.5hours
+
+Start time:630
+
+Finish time: 730
+
+Actual time needed to complete: 1hour
+
+
+
+Number and name of feature:feature 2 get weather
+
+Estimate of time needed to complete: 1.5hours
+
+Start time: 730
+
+Finish time: 
+
+Actual time needed to complete: 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 //node module dependencies
+let allWeather = [];
 require('dotenv').config();
 const express = require('express');
 //app
@@ -10,7 +11,12 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
 // app.use(express.static('./public'));
-
+//borroed from stack overflow from epoc
+function toDateTime(secs) {
+  var t = new Date(1970, 0, 1); // Epoch
+  t.setSeconds(secs);
+  return t;
+}
 // app.get('/hello', (request, response) =>{
 //   response.status(200).send('hello');
 // })
@@ -27,14 +33,35 @@ function searchToLatLong(query){
   return location;
 }
 
+
+
 function Location(location){
   this.formatted_query = location.formatted_address;
   this.latitude = location.geometry.location.lat;
   this.longitude = location.geometry.location.lng;
 }
+app.get('/weather', (request, response) => {
+  console.log(request);
+  const weatherData = searchWeather(request.query.data || 'Lynnwood, WA');
+  response.send(weatherData);
+});
+
+function searchWeather(query){
+  allWeather = [];
+  const wData = require('./weather.json');
+  wData.daily.data.forEach((day) => {
+    new Weather(day);
+  });
+  return(allWeather);
+}
+function Weather(day){
+  this.forcast = day.summary;
+  this.time = new Date (day.time * 1000).toGMTString();
+  allWeather.push(this);
+}
 //errors
-app.use('/*', (req, res) =>{
-  res.status(400).send(`sorry that didn't work`);
+app.get('/*', (req, res) =>{
+  res.status(404).send(`sorry that didn't work`);
 })
 
 app.listen(PORT, ()=> console.log(`listening on port ${PORT} ya mook`));
